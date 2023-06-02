bd2335f26faf374cc762c07319a16d16387e6b29
Daniel <danielmtdc16@gmail.com>
2023-06-02 12:20:34 -0300

    script finalizado.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const capital_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lower_letters = "abcdefghijkmnopqrstuvwxyz"
 const special_characters = "@#$%&*()<>/=+-_.";
 const numbers = "0123456789";
+const list_name_checkbox = [capital_letter, lower_letter, special_character, number];
+const list_elements = [capital_letters, lower_letters, special_characters, numbers];
 
 function randomValue(str) {
     return Math.floor(Math.random() * str.length);
@@ -22,16 +24,26 @@ function character_generate(checkbox, elements) {
     }
 }
 
-password_length.value = 8;
-
 function generate() {
 
-    if ((password_length.value != "" && password_length.value != 0) && (special_character.checked || capital_letter.checked || lower_letter.checked || number.checked)) {
+    if (password_length.value < 8 || password_length.value == "") password_length.value = 8;
+
+    if (special_character.checked || capital_letter.checked || lower_letter.checked || number.checked) {
         
         let password = "";
+        let i = 0;
+        while (password.length < password_length.value) {
+            if (i == 4){
+                i = 0;
+            }
+            password += character_generate(list_name_checkbox[i], list_elements[i]);
+            i++;
+        }
+
+        result_field.innerHTML = password;
 
     } else {
-        alert("- A quantidade de caracteres deve ser maior ou igual a 8\n- Marque pelo menos uma caixa");
+        alert("- Marque pelo menos uma caixa");
     }
 
 }
